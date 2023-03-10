f41e9ce77a7d3cd9c9330ffb695b587d681dda65
Strahinja <strale.alek@gmail.com>
2023-03-10 12:09:01 +0100

    Implement the "Complete" button

--- gui.py
+++ gui.py
@@ -6,9 +6,10 @@ input_box = Sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = Sg.Button("Add")
 list_box = Sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
 edit_button = Sg.Button("Edit")
+complete_button = Sg.Button("Complete")
 
 window = Sg.Window("Todo app",
-                   layout=[[label],  [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],  [input_box, add_button], [list_box, edit_button, complete_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -32,6 +33,14 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todo']
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
