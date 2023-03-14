ec30917af070faeeda60b7297d5b5b182d035540
Strahinja <strale.alek@gmail.com>
2023-03-14 21:57:51 +0100

    Implement "Add new todo items" feature

--- WebGui.py
+++ WebGui.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title("To do")
 
-todos = functions.get_todos()
 
 for todo in todos:
     st.checkbox(todo)
 
-
-st.text_input(label="", placeholder="Enter a todo")
+st.text_input(label="", placeholder="Enter a todo", on_change=add_todo, key="new_todo")
